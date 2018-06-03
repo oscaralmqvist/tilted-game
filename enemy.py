23e864be3d026a2b253aeaff2414ebc2cd7c1d2e
Oscar Almqvist <almqvist.oscar@gmail.com>
2018-06-03 18:57:54 +0200

    Removed unused moved classes

--- enemy.py
+++
@@ -1,9 +0,0 @@
-import pygame
-from pygame.locals import *
-from player import *
-
-class Enemy:
-    def __init__(self):
-        self.img = pygame.image.load("resources/enemy_img.png")
-        self.x_pos = 20
-        self.y_pos = 20;
